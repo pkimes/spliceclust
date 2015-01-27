3650d6e815ad555669f9161165fd6f2e83404bff
pkimes <patrick.kimes@gmail.com>
2015-01-27 14:59:03 -0500

    Initialize test directory

---
+++ tests/testthat/test-splicegrahm.R
@@ -0,0 +1,9 @@
+context("splicegrahm plotting method")
+
+
+test_that("splicegrahm default call works properly", {
+    ## 1. load test data file
+    ## 2. expect_is(splicegrahm(cc), "ggplot")
+    expect_is(TRUE, "logical")
+})
+
