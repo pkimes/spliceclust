e6f5b9950b651b499ad1ebd306ac5da005e706e8
pkimes <patrick.kimes@gmail.com>
2015-03-17 15:00:41 -0400

    Remove unused tests to pass build

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library("testthat")
-library("spliceclust")
-
-test_check("spliceclust")
--- tests/testthat/test-splicegrahm.R
+++
@@ -1,9 +0,0 @@
-context("splicegrahm plotting method")
-
-
-test_that("splicegrahm default call works properly", {
-    ## 1. load test data file
-    ## 2. expect_is(splicegrahm(cc), "ggplot")
-    expect_is(TRUE, "logical")
-})
-
