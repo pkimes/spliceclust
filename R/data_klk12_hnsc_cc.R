9ddae90058c181cce46d6cb7b7e92a60b336b69f
pkimes <patrick.kimes@gmail.com>
2016-03-03 09:07:41 -0800

    Include and document dataset used in README

---
+++ R/data_klk12_hnsc_cc.R
@@ -0,0 +1,13 @@
+#' connected component of KLK12 Expression in HNSC 
+#'
+#' concomp object of exon and splice junction expression values
+#' for a cohort of 279 head and neck squamous cell (HNSC) samples
+#' with 16 exons and 16 junctions.
+#'
+#' @docType data
+#' @usage data(klk12_hnsc_cc)
+#' @format a concomp object
+#' @source The Cancer Genome Atlas Project
+#' @name klk12_hnsc_cc
+#' @author Patrick Kimes
+NULL
---
+++ R/data_klk12_lusc_gr.R
@@ -0,0 +1,13 @@
+#' GRanges of KLK12 Expression in LUSC 
+#'
+#' GRanges object of exon and splice junction boundaries with metadata columns
+#' providing exon/junction labels and expression values for a cohort of 177
+#' lung sequamous cell (LUSC) samples with 13 exons and 12 junctions.
+#'
+#' @docType data
+#' @usage data(klk12_lusc_gr)
+#' @format a GRanges object
+#' @source The Cancer Genome Atlas Project
+#' @name klk12_lusc_gr
+#' @author Patrick Kimes
+NULL
