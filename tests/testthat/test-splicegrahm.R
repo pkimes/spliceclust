2ea48d0112069fdff61da62cdb15b86aa1aac7c3
pkimes <patrick.kimes@gmail.com>
2016-10-26 00:16:14 -0700

    Add more splicegrahm test cases

--- tests/testthat/test-splicegrahm.R
+++ tests/testthat/test-splicegrahm.R
@@ -156,46 +156,105 @@ test_that("splicegrahm accepts sort_idx to maually specify sort order", {
 
 test_that("splicegrahm accepts log_base, log_shift to scale heatmap colorscale", {
     ## plot with simple dataset
-    expect_silent(plt <- splicegrahm(simple_cc, j_incl = TRUE, log_base = 2, log_shift = 1))
+    expect_silent(plt_b2s1 <- splicegrahm(simple_cc, j_incl=TRUE, log_base=2, log_shift=1))
+    expect_silent(plt_b2s0 <- splicegrahm(simple_cc, j_incl=TRUE, log_base=2, log_shift=0))
+    expect_silent(plt_b10s1 <- splicegrahm(simple_cc, j_incl=TRUE, log_base=10, log_shift=1))
+    expect_silent(plt_b10s0 <- splicegrahm(simple_cc, j_incl=TRUE, log_base=10, log_shift=0))
+    ## plot without log scaling (log_base=0)
+    expect_silent(plt_b0s1 <- splicegrahm(simple_cc, j_incl=TRUE, log_base=0, log_shift=1))
+    expect_silent(plt_b0s0 <- splicegrahm(simple_cc, j_incl=TRUE, log_base=0, log_shift=0))
 
     ## check that ggplot is produced
-    expect_is(plt, "ggplot")
-    bld <- ggplot_build(plt)
+    expect_is(plt_b2s1, "ggplot")
+    expect_is(plt_b2s0, "ggplot")
+    expect_is(plt_b10s1, "ggplot")
+    expect_is(plt_b10s0, "ggplot")
+    expect_is(plt_b0s1, "ggplot")
+    expect_is(plt_b0s0, "ggplot")
+
+    ## check that all scales are still discrete
+    expect_is(plt_b2s0$scales$scales[[4]], "ScaleDiscrete")
+    expect_is(plt_b2s1$scales$scales[[4]], "ScaleDiscrete")
+    expect_is(plt_b10s0$scales$scales[[4]], "ScaleDiscrete")
+    expect_is(plt_b10s1$scales$scales[[4]], "ScaleDiscrete")
+    ## check that scales are changed to continuous if non-log plotting
+    expect_is(plt_b0s0$scales$scales[[4]], "ScaleContinuous")
+    expect_is(plt_b0s1$scales$scales[[4]], "ScaleContinuous")
+
+    ## check that non-log scaling ignores shift param
+    expect_equal(plt_b0s0$data, plt_b0s1$data)
     
+    ## check that scales are changed
+    expect_equal(plt_b2s1$scales$scales[[4]]$labels,
+                 paste0("<", 2^(1:11))) 
+    expect_equal(plt_b2s0$scales$scales[[4]]$labels,
+                 paste0("<", 2^(1:11))) 
+    expect_equal(plt_b10s1$scales$scales[[4]]$labels,
+                 paste0("<", 10^(1:4))) 
+    expect_equal(plt_b10s0$scales$scales[[4]]$labels,
+                 paste0("<", 10^(1:4))) 
 })
 
 
 test_that("splicegrahm accepts bin FALSE to plot on continuous colorscale", {
     ## default plot with simple dataset
-    plt <- splicegrahm(simple_cc)
+    expect_silent(plt_nobin <- splicegrahm(simple_cc, j_incl= TRUE, bin = FALSE, log_base=2))
+    expect_silent(plt_wbin <- splicegrahm(simple_cc, j_incl= TRUE, bin = TRUE, log_base=2))
 
-    ## check that ggplot is produced
-    expect_is(plt, "ggplot")
-    bld <- ggplot_build(plt)
+    ## check that ggplots are produced
+    expect_is(plt_nobin, "ggplot")
+    expect_is(plt_wbin, "ggplot")
+    bld_nobin <- ggplot_build(plt_nobin)
+    bld_wbin <- ggplot_build(plt_wbin)
 
+    ## check that color scales is discrete only if binning
+    expect_is(plt_nobin$scales$scales[[4]], "ScaleContinuous")
+    expect_is(plt_wbin$scales$scales[[4]], "ScaleDiscrete")
+    
+    ## check that value for color is factor only if binning
+    expect_false(is.factor(plt_nobin$data$value))
+    expect_true(is.factor(plt_wbin$data$value))
 })
 
-#' #' @param log_base a numeric specifying the scale of the binning for the
-#' #'        plotting of expression values at each exon, which 0 resulting in no long
-#' #'        scaling being applied (default = 10)
-#' #' @param log_shift a numeric specifying the shift to be used in the log transformation
-#' #'        for handling 0 values (default = 1)
-#' #' @param bin a logical whether to bin the values for easier viewing (default = TRUE)
 
 test_that("splicegrahm accepts genomic, ex_use input to adjust x-axis scaling", {
     ## plot with simple dataset
-    plt <- splicegrahm(simple_cc)
+    expect_silent(plt_gen <- splicegrahm(simple_cc, genomic = TRUE))
+    expect_silent(plt_non <- splicegrahm(simple_cc, genomic = FALSE))
+    expect_silent(plt_non_10 <- splicegrahm(simple_cc, genomic = FALSE, ex_use = 1.0))
+
+    ## plot with ex_use less than exon proportion in genomic space
+    expect_warning(plt_non_01 <- splicegrahm(simple_cc, genomic = FALSE, ex_use = 0.1),
+                   paste0("Using 'genomic = TRUE' since exons occupy more than specified 'ex_use' ",
+                          "proportion of plot in genomic coordinates. No need to squish."))
 
     ## check that ggplot is produced
-    expect_is(plt, "ggplot")
-    bld <- ggplot_build(plt)
+    expect_is(plt_gen, "ggplot")
+    expect_is(plt_non, "ggplot")
+    expect_is(plt_non_10, "ggplot")
+    expect_is(plt_non_01, "ggplot")
+    bld_gen <- ggplot_build(plt_gen)
+    bld_non <- ggplot_build(plt_non)
+    bld_non_10 <- ggplot_build(plt_non_10)
+    bld_non_01 <- ggplot_build(plt_non_01)
 
+    ## check that coordinate range shrinks in expected proportions
+    width_gen <- diff(bld_gen$panel$ranges[[1]]$x.range)
+    width_non <- diff(bld_non$panel$ranges[[1]]$x.range)
+    width_non_10 <- diff(bld_non_10$panel$ranges[[1]]$x.range)
+    expect_lt(width_non, width_gen)
+    expect_lt(width_non_10, width_gen)
+    expect_equal(width_non_10 / width_non, 2/3, tolerance=0.001)
+    
+    ## check that bad (small) ex_use creates same plot as genomic = TRUE
+    expect_equal(bld_gen$data, bld_non_01$data)
+    expect_equal(bld_gen$panel, bld_non_01$panel)
+    expect_equal(plt_gen[names(plt_gen) != "plot_env"],
+                 plt_non_01[names(plt_non_01) != "plot_env"])
+
+    ## add tests for when txlist specified
 })
 
-#' #' @param genomic a logical whether genomic coordinates should be used to
-#' #'        plot the heatmap (default = TRUE)
-#' #' @param ex_use a numeric specifying the proportion of the plot exons should occupy if
-#' #'        non-genomic coordinate plotting is desired (default = 2/3)
 
 test_that("splicegrahm accepts flip_neg to adjust whether stranded-ness matters", {
     ## default plot with simple dataset
